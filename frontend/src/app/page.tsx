22257c59891fbe96333563ba56ee22b178776c03
Sankalp Chudmunge <csankalp21@gmail.com>
2026-01-07 00:07:43 +0530

    date overlap fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -351,7 +351,7 @@ const MenttorLabsMainPage = () => {
                       <div key={module.id} className="relative pl-8 sm:pl-32 py-6 group">
                         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-200 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-auto h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full px-2">{`${module.startDate} - ${module.endDate}`}</time>
-                          <div className="text-xl font-bold text-slate-900">{`Module ${moduleIndex + 1}: ${module.title}`}</div>
+                          <div className="text-xl font-bold text-slate-900 sm:ml-40">{`Module ${moduleIndex + 1}: ${module.title}`}</div>
                         </div>
                         <div className="ml-8 sm:ml-32 space-y-4">
                           {module.topics.map((topic) => (
@@ -372,7 +372,7 @@ const MenttorLabsMainPage = () => {
                       <div key={module.id} className="relative pl-8 sm:pl-32 py-6 group">
                         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-200 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                           <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">{module.timeline}</time>
-                          <div className="text-xl font-bold text-slate-900">{`Module ${moduleIndex + 1}: ${module.title}`}</div>
+                          <div className="text-xl font-bold text-slate-900 sm:ml-24">{`Module ${moduleIndex + 1}: ${module.title}`}</div>
                         </div>
                         <div className="ml-8 sm:ml-32 space-y-4">
                           {module.topics.map((topic) => (
